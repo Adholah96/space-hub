19e2eee2f40d72ddc2cebc44d718523eff23b280
Adholah96 <adholah96@gmail.com>
2023-04-11 21:11:55 +0300

    Add basic styling

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import RocketItem from './RocketItem';
 
 function Rockets() {
   return (
-    <div>Rockets</div>
+    <div className="rocket-holder">
+      <RocketItem />
+      <RocketItem />
+      <RocketItem />
+    </div>
   );
 }
 
