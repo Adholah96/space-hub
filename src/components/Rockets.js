eff06cbde37490aeab83c8d418f008bfc15326b4
Adholah96 <adholah96@gmail.com>
2023-04-12 10:45:45 +0300

    display rocket list

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getRockets } from 'redux/rockets/rocketsSlice';
 import RocketItem from './RocketItem';
 
 function Rockets() {
+  const dispatch = useDispatch();
+  const rocketsData = useSelector((store) => store.rockets);
+  const fetch = useRef(true);
+
+  useEffect(() => {
+    if (fetch.current) {
+      if (rocketsData.length === 0) {
+        fetch.current = false;
+        dispatch(getRockets());
+      }
+    }
+  }, [dispatch]);
+
   return (
     <div className="rocket-holder">
-      <RocketItem />
-      <RocketItem />
-      <RocketItem />
+      {rocketsData.map((rocket) => (
+        <RocketItem
+          key={rocket.id}
+          name={rocket.name}
+          description={rocket.description}
+          img={rocket.flickr_image}
+          id={rocket.id}
+          reserved={rocket.reserved}
+        />
+      ))}
     </div>
   );
 }
