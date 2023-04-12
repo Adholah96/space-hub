49278b413a076a7ff3343371f0b2c77b6b2112b6
Musong Ernest Akeh <ernestakeh@gmail.com>
2023-04-12 21:04:53 +0100

    add joined missions to profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import '../styles/Profile.css';
 
 function Profile() {
+  const { missions } = useSelector((store) => store.missions);
+  const filteredMissions = missions.filter((mission) => mission.reserved === true);
   return (
-    <div>Profile</div>
+    <main>
+      <div className="missions">
+        <h3>my missions</h3>
+        <ul>
+          { filteredMissions.map((mission) => {
+            const id = mission.mission_id;
+            const name = mission.mission_name;
+            return <li key={id}>{name}</li>;
+          }) }
+        </ul>
+      </div>
+    </main>
   );
 }
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import missionsReducer from './missions/missionsSlice';
 import rocketsReducer from './rockets/rocketsSlice';
-import profileReducer from './profile/profileSlice';
 
 const store = configureStore({
   reducer: {
     missions: missionsReducer,
     rockets: rocketsReducer,
-    profile: profileReducer,
   },
 });
 
