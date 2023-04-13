89884c6cc538ac1f6c4bfdd87c20466fcfd231ac
Musong Ernest Akeh <ernestakeh@gmail.com>
2023-04-13 08:31:47 +0100

    add rockets to profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -4,7 +4,9 @@ import '../styles/Profile.css';
 
 function Profile() {
   const { missions } = useSelector((store) => store.missions);
+  const rockets = useSelector((store) => store.rockets);
   const filteredMissions = missions.filter((mission) => mission.reserved === true);
+  const filteredRockets = rockets.filter((rocket) => rocket.reserved === true);
   return (
     <main>
       <div className="missions">
@@ -14,6 +16,15 @@ function Profile() {
             const id = mission.mission_id;
             const name = mission.mission_name;
             return <li key={id}>{name}</li>;
+          }) }
+        </ul>
+      </div>
+      <div className="missions">
+        <h3>my rockets</h3>
+        <ul>
+          { filteredRockets.map((rocket) => {
+            const rocketId = rocket.id;
+            return <li key={rocketId}>{rocket.description}</li>;
           }) }
         </ul>
       </div>
